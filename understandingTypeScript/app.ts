c57d8a785e557918583b726fe667ec1c90cb15f9
saurabhWebr <saurabhsp317@gmail.com>
2024-01-21 21:35:14 +0530

    covered basics types

--- understandingTypeScript/app.ts
+++ understandingTypeScript/app.ts
@@ -1,38 +1,15 @@
-// ***************OBJECT*****************
-// const person = {
-//   name: "saurabh",
-//   age: 23
-// };
-// console.log(person.name, typeof(person));
+//***************unknown type*****************
+// let userInput : unknown // we don't know that which value we will be storing
+// userInput = "joel";
+// console.log(userInput);
 
-// const person: { // here this new object assignment where we mentioned the types
-//   name: string; // is basically same as assigning "object" keyword
-//   age: number;
-// } = {
-//   name: "saurabh",
-//   age: 23
-// };
-// //in ts object types contains key and type pairs unlike js where we have key and value pair. 
-// console.log(person.name);
-
-// ******************ARRAY********************
-
-// const person = {
-//   name: "saurabh",
-//   age: 23,
-//   hobbies: ["dancing", "cooking"] // here this array is of string type
-// };
-// // as here the hobbies is an array of string type so we can also use string methods on each element of the hobbies
-// for(const hobby of person.hobbies) {
-//   console.log(hobby.toUpperCase())
+//**************never type*******************
+// //it states that a function will never return anything
+// function generateError (msg : string) : never {
+//   throw {message : msg}
 // }
+// console.log(generateError("request failed"))
 
-//*******************TUPLES*********************
-// if we know the type of values to be stored in array and also the length of the array then tupples can be very handy 
-// for adding more strictness
 
-// const details = ["manager", 25000] // here ts is not sure the type so its inference type is either string or number
-// let detailsArray : [string , number] // so we can do something like that (this is tuple) so now we cannot assign any thhing other than string to the arr[0]
-// detailsArray = ["manager", 3]
-// console.log(detailsArray)
+
 
---
+++ understandingTypeScript/unionsAndAlias.ts
@@ -0,0 +1,27 @@
+// //*****************UNIONS*******************/
+// // IT is very helpful while we want to set type of a variable or a parameter to more than one type e.g number or string
+// function add (num1: numberString, num2: numberString) {
+//   let result: any;
+//   if (typeof(num1) === 'number' && typeof(num2) === "number") {
+//     result = num1+num2;
+//     return result;
+//   } else {
+//     result = num1.toString() + num2.toString();
+//     return result
+//   }
+
+// }
+
+// const number1 = 5;
+// const number2 = 8;
+// console.log(add(+number1, +number2));
+// const firstName = "Saurabh";
+// const lastName = "Pandey";
+// console.log(add(firstName, lastName));
+// //*********************Type alias**************** */
+// //it increases the code readablity, we can assign our unions to this see the example above
+// type numberString = number | string
+
+// let a : string;
+// a = "aman";
+// console.log(a);
